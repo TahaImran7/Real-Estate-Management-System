a1e749efe641d9386c3521257a606128fbc3f0f3
TahaImran7 <tahaimran315@gmail.com>
2025-10-31 01:33:43 +0500

    Added required files and folders

---
+++ testdb.php
@@ -0,0 +1,9 @@
+<?php
+include 'includes/db_connect.php';
+
+$sql = "SELECT DATABASE()";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_array($result);
+
+echo "✅ Connected to database: " . $row[0];
+?>
